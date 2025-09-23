1f80db38023b15b1fd79e4706c59a43c1a0d1029
Moises Núñez Ibarra <mizulegendsgg@gmail.com>
2025-09-23 23:54:26 +0000

    Update config.js

--- docs/js/config.js
+++ docs/js/config.js
@@ -21,7 +21,7 @@ let moveHistory = [];
 let moveCount = 0;
 let voxelCount = 0;
 
-// Variables multijugador
+// Variables multijugador (declaradas aquí para evitar redeclaración)
 let peer = null;
 let isHost = false;
 let roomCode = null;
@@ -29,9 +29,31 @@ const connections = {};
 let localPeerId = null;
 let myRoleLocal = 'spectator';
 
-// Exponer variables globalmente
+// Exponer variables y configuración globalmente
 window.CONFIG = CONFIG;
 window.scene = scene;
 window.camera = camera;
 window.renderer = renderer;
-// ... y así con todas las variables necesarias
+window.sunLight = sunLight;
+window.moonLight = moonLight;
+window.ambientLight = ambientLight;
+window.stars = stars;
+window.dayTime = dayTime;
+window.board = board;
+window.pieces = pieces;
+window.selectedPiece = selectedPiece;
+window.validMoves = validMoves;
+window.currentTurn = currentTurn;
+window.capturedPieces = capturedPieces;
+window.gameStatus = gameStatus;
+window.moveHistory = moveHistory;
+window.moveCount = moveCount;
+window.voxelCount = voxelCount;
+
+// Exponer variables multijugador
+window.peer = peer;
+window.isHost = isHost;
+window.roomCode = roomCode;
+window.connections = connections;
+window.localPeerId = localPeerId;
+window.myRoleLocal = myRoleLocal;
