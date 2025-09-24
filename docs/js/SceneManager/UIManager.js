f93f4776f186cadfaad71f661d1f2fa1c4cc0fe2
Moises Núñez Ibarra <mizulegendsgg@gmail.com>
2025-09-24 09:49:30 +0000

    Create UIManager.js

---
+++ docs/js/SceneManager/UIManager.js
@@ -0,0 +1,22 @@
+// js/SceneManager/UIManager.js
+class UIManager {
+    constructor() {
+        this.dayTimeElement = document.getElementById('dayTime');
+        this.timeDisplayElement = document.getElementById('timeDisplay');
+    }
+    
+    updateDayTime(period) {
+        if (this.dayTimeElement) {
+            this.dayTimeElement.textContent = period;
+        }
+    }
+    
+    updateTimeDisplay(hours, minutes) {
+        if (this.timeDisplayElement) {
+            this.timeDisplayElement.textContent = 
+                `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}`;
+        }
+    }
+}
+
+export { UIManager };
